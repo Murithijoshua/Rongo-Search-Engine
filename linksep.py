4396ddd860b3f145763e2686a1e7f53536a241f8
jcodes <brodiamyzalius@gmail.com>
2022-01-06 22:41:13 +0300

    working on links

--- linksep.py
+++ linksep.py
@@ -1,6 +1,10 @@
-with open('/home/jj/projects/search/search/links.txt', 'r') as r:
-    data = r.read()
-    data =set(data.split(","))
-with open('/home/jj/projects/search/search/links.txt', 'w') as r:
-    for i in data:
-        r.write(i + '\n')
+def separatelinks():
+    with open('/home/jj/projects/search/search/links.txt', 'r') as r:
+        data = r.read()
+        data =data.split(",")
+        data = set(data)
+    with open('/home/jj/projects/search/search/links.txt', 'w') as r:
+        for i in data:
+            r.write(i + '\n')
+            
+separatelinks()
